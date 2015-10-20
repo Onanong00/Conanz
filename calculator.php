4c5522b11088a5197b018f841129b87a3d6d1401
Onanong Ninlaor <nanyippy@hotmail.com>
2015-10-20 04:04:39 -0400

    Uplond file

---
+++ calculator.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title>Calculator</title>
+</head>
+<body>
+<center style="border:#33FFF 2px SOLTD">
+<br><font size="10"> Calculator.</font></center>
+<br><center><form method='post' action='calculator.php'>
+<input type='text' name='valuel'>
+<input type='text' name='value2'>
+
+ <select name='action'>
+<option>+</option>
+<option>-</option>
+<option>*</option>
+<option>/</option>
+</select>
+<input type='submit' name='submit' value='Calculate Now'>
+</form></center>
+<center><?php
+      if(isset($_POST['submit'])){
+
+	    $valuel = $_POST['valuel'];
+      $value2= $_POST['value2'];
+			$action = $_POST['action'];
+			
+      if($action=="+"){
+		  echo "Your Answer is:<br>";
+			echo $valuel+$value2;
+			}
+
+			if($action=="-"){
+			echo "Your Answer is:<br>";
+			echo $valuel-$value2;
+			}
+
+			if($action=="*"){
+			echo "Your Answer is:<br>";
+			echo $valuel*$value2;
+			}
+
+			if($action=="/"){
+			echo "Your Answer is:<br>";
+			echo $valuel/$value2;
+			}
+
+}
+?></center>
+</body>
+</html>
